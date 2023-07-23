df93885823599beb20c9aa2a680f1a7516986232
deterquetejoder <saecularisator@gmail.com>
2023-07-23 20:47:31 +0500

    ts10 done

---
+++ ts10/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Type your number:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num >=99 && num <=1000)
+{
+    int x = (num % 100 - num % 10) / 10;
+    Console.WriteLine(x);
+}
+else 
+{
+    Console.WriteLine("This is not a three-digit number.");
+
+}
