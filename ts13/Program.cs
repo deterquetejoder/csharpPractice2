b1f5696e1694dc1ddbbf0e3e54167d935f77db35
deterquetejoder <saecularisator@gmail.com>
2023-07-25 19:09:31 +0500

    ts13 works with+

--- ts13/Program.cs
+++ ts13/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+Console.WriteLine("Type your number:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+List<int> digits = new List<int>();
+
+foreach (char c in num.ToString())
+{
+    int digit = int.Parse(c.ToString());
+    digits.Add(digit);
+}
+
+int lg = digits.Count();
+if (lg >= 3)
+{
+int ThDigit = digits[2];
+Console.WriteLine(ThDigit);
+}
+else
+{
+  Console.WriteLine("There is no third digit found "); // If the input is less than or equal to one, there are
+}
