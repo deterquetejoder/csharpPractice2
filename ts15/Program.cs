424c5a0f85abef7b35856950c7f967064407a42d
deterquetejoder <saecularisator@gmail.com>
2023-08-02 19:11:54 +0500

    ts15 fixed

--- ts15/Program.cs
+++ ts15/Program.cs
@@ -2,10 +2,21 @@
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 string[] weekDays = new string[] { 
-     "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"
+     "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Satturday", "Sunday"
      };
 
 Console.WriteLine("Enter the number ");
 int inp = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine(weekDays[inp-1]);
+if(inp > 5 && inp < 8)
+{
+Console.WriteLine($"yes, {weekDays[inp-1]} is a weekend!");
+}
+else if (inp>0 && inp< 6)
+{
+    Console.WriteLine($"No, {weekDays[inp-1]} is a working day..");
+}
+else
+{
+    Console.WriteLine("There is no this day on a week");
+}
