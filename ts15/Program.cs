a9384086c6f3f88ffcea6a6968566012e0be3c9d
deterquetejoder <saecularisator@gmail.com>
2023-08-02 18:41:52 +0500

    ts15 works

--- ts15/Program.cs
+++ ts15/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+string[] weekDays = new string[] { 
+     "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"
+     };
+
+Console.WriteLine("Enter the number ");
+int inp = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(weekDays[inp-1]);
